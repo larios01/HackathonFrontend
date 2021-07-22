2374e06ec83932f0430448e8438a388bef2f417f
anthony escalante <anthony.escalante@revature.net>
2021-07-22 19:18:14 -0400

    create complete

--- HackathonFront/src/app/create-character/create-character.component.ts
+++ HackathonFront/src/app/create-character/create-character.component.ts
@@ -22,13 +22,16 @@ export class CreateCharacterComponent implements OnInit {
   }
 
   onSubmit(){
+    this.character.gender=(<HTMLInputElement>document.getElementById("gender")).value;
+    this.character.hairColor=(<HTMLInputElement>document.getElementById("hairColor")).value;
+    this.character.eyeColor=(<HTMLInputElement>document.getElementById("eyes")).value;
     this.character.race=(<HTMLInputElement>document.getElementById("race")).value;
-    this.character.strength=Number((<HTMLInputElement>document.getElementById("strength")).value);
-    this.character.intelligent=Number((<HTMLInputElement>document.getElementById("intelligent")).value);
-    this.character.wisdom=Number((<HTMLInputElement>document.getElementById("wisdom")).value);
-    this.character.dexterity=Number((<HTMLInputElement>document.getElementById("dexterity")).value);
-    this.character.luck=Number((<HTMLInputElement>document.getElementById("luck")).value);
-    this.character.numOfToes=Number((<HTMLInputElement>document.getElementById("toeNum")).value);
+    console.log(Number((<HTMLInputElement>document.getElementById("strength")).value));
+    console.log(Number((<HTMLInputElement>document.getElementById("intelligent")).value));
+    console.log(Number((<HTMLInputElement>document.getElementById("wisdom")).value));
+    console.log(Number((<HTMLInputElement>document.getElementById("dexterity")).value));
+    console.log(Number((<HTMLInputElement>document.getElementById("luck")).value));
+    console.log(Number((<HTMLInputElement>document.getElementById("toeNum")).value));
     // this.characterService.createCharacter(this.character).subscribe(res => {
       console.log(this.character);
     // });
