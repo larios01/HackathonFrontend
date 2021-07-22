9b80797940eeb29a913c61739b25c02b9598d266
rss32 <write_me_later@hotmail.com>
2021-07-22 16:10:23 -0400

    added functionality to back button

--- HackathonFront/src/app/view-character/view-character.component.ts
+++ HackathonFront/src/app/view-character/view-character.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-view-character',
@@ -7,25 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewCharacterComponent implements OnInit {
 
-  public characterName?:string;
+  public characterName?: string;
 
-  public gender?:string;
+  public gender?: string;
   public hairColor?: string;
-  public strength?:string;
-  public intelligence?:string;
+  public strength?: string;
+  public intelligence?: string;
   public wisdom?: string;
-  public dexterity?:string;
-  public numToes?:string;
-  public eyeColor?:string;
-  public race?:string;
+  public dexterity?: string;
+  public numToes?: string;
+  public eyeColor?: string;
+  public race?: string;
   public luck?: string;
 
-  public description?:string;
+  public description?: string;
 
-  constructor() { }
+  constructor(private location: Location, private router: ActivatedRoute) { }
 
-  
+
   ngOnInit(): void {
-  }
 
+    //Get the id of the character from the url so that we can fetch data
+    let characterIdString = this.router.snapshot.paramMap.get('id');
+    let characterId: number = 0;
+    if (characterIdString != null) {
+      characterId = parseInt(characterIdString);
+    }
+  }
+  
+  goBack(): void {
+    this.location.back();
+  }
 }
